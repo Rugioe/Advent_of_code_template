f906224040ad89c5d92b53d3b005e7522a8a3bbe
Rugioe <julian.kemp@outlook.de>
2021-12-15 08:51:30 +0100

    Initial commit

---
+++ src/main copy.rs
@@ -0,0 +1,4 @@
+const INPUT_TEST: &str = "";
+
+
+const INPUT: &str = "";
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+// Modules
+mod constants;
+use constants::*;
+
+fn main() {
+    //println!("{}", INPUT);
+
+    part_a();
+    //part_b();
+}
+
+fn part_a() {
+
+    //println!("{}", );
+}
+
+fn part_b() {
+
+}
