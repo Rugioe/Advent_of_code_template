07daefc27dc7b09923d63073fc6fa113317266b4
Julian Kemp <julian.kemp@outlook.de>
2021-12-19 17:25:28 +0100

    Elapsed time + stl comment

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,18 @@
 mod constants;
 use constants::*;
 
+//Stl
+use std::time::Instant;
+
 fn main() {
+    let start_instant = Instant::now();
+
     //println!("{}", INPUT);
 
     part_a();
     //part_b();
+
+    println!("Elapsed time: {}", start_instant.elapsed().as_secs_f32());
 }
 
 fn part_a() {
